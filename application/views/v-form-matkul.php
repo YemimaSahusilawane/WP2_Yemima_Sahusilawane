eb0777de817a9ea76ae731616e7b301156ae5084
YemimaSahusilawane <yemimasahu@gmail.com>
2021-10-06 13:37:57 +0700

    mengedit autoload dan menambahkan file form validation

---
+++ application/views/v-form-matkul.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+  <title>Form Input Mata Kuliah</title>
+</head>
+<body>
+  <center>
+    <form action="<?= base_url('matakuliah/cetak')?>"method="post">
+    <table>
+      <tr>
+        <th colspan="3">
+          Form Input Data Mata Kuliah
+        </th>
+      </tr>
+      <tr>
+        <td colspan="3">
+          <hr>
+        </td>
+      </tr>
+      <tr>
+        <th>Kode MTK</th>
+        <td>:</td>
+        <td>
+          <input type="text" name="kode" id="kode">
+        </td>
+      </tr>
+      <tr>
+        <th>Nama MTK</th>
+        <td>:</td>
+        <td>
+          <input type="text" name="nama" id="nama">
+        </td>
+      </tr>
+      <tr>
+        <th>SKS</th>
+        <td>:</td>
+        <td>
+          <select name="sks" id="sks">
+            <option value="">Pilih SKS</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="3" align="center">
+          <input type="text" name="submit" id="Submit">
+        </td>
+      </tr>
+    </table>
+  </form>
+  </center>
+</body>
+</html>
